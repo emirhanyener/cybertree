0a5e8beadf11e679a7a5c5271084bd18c4e394a8
emirhanyener <yeneremirhann@gmail.com>
2022-11-26 18:51:58 +0300

    update main: add other controls

--- cyborg.cpp
+++ cyborg.cpp
@@ -4,6 +4,8 @@
 using namespace std;
   
 bool isKeyword(string);
+bool isOperator(string);
+bool isInteger(string);
 
 int main(int argc, char** argv)
 {
@@ -11,16 +13,23 @@ int main(int argc, char** argv)
   string lex = "";
   ifstream file("program.cy");
   while (getline (file, text)) {
-    for(int i = 0; i <= text.length(); i++){
-      if(text[i] != ' ' && text[i] != '.'){
+    for(int i = 0; i <= text.length() - 1; i++){
+      if(text[i] != ' ' && text[i] != '.' && text[i] != '\n' && text[i] != 0){
         lex+=text[i];
       }else{
         if(isKeyword(lex))
-          cout << lex << " is a keyword" << "\n";
+          cout << "\"" << lex << "\"" << " is a keyword" << "\n";
+        else if(isInteger(lex))
+          cout << "\"" << lex << "\"" << " is a integer" << "\n";
+        else if(isOperator(lex))
+          cout << "\"" << lex << "\"" << " is a operator" << "\n";
         else
-          cout << lex << " is not a keyword" << "\n";
+          cout << "\"" << lex << "\"" << " is a not identified" << "\n";
 
         lex = "";
+      }
+      if(text[i] == '.'){
+        cout << "\".\" is a seperator" << "\n";
       }
     }
   }
@@ -41,4 +50,23 @@ bool isKeyword(string lex){
     return true;
   }
   return false;
+}
+bool isOperator(string lex){
+  if(
+  lex[0]=='+' || 
+  lex[0]=='-' || 
+  lex[0]=='*' || 
+  lex[0]=='/'
+  ){
+    return true;
+  }
+  return false;
+}
+bool isInteger(string lex){
+  for (int i = 0; i < lex.length(); i++) {
+    if(lex[i] <= '0' || lex[i] >= '9'){
+      return false;
+    }
+  }
+  return true;
 }
