e063659a7c9225b38d75af31b765366ee6eec1ec
emirhanyener <yeneremirhann@gmail.com>
2022-11-26 17:44:00 +0300

    update main: file read

--- cyborg.cpp
+++ cyborg.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 using namespace std;
   
 int main(int argc, char** argv)
 {
-  
+  string text;
+  ifstream file("program.cy");
+  while (getline (file, text)) {
+    cout << text;
+  }
+  file.close();
 
   return 0;
 }
