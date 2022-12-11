b6765d3e887305d61c23841b3149702d007480eb
emirhanyener <yeneremirhann@gmail.com>
2022-12-11 13:10:33 +0300

    update grammar

--- grammar.cpp
+++ grammar.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#define grammarCount 15
 
 class grammar{
     public:
-    string grammars[13][2];
+    const int grammarNum = grammarCount;
+    string grammars[grammarCount][2];
 
     grammar(){
         ////////////////////////////////////////////////////////////////////////
@@ -22,7 +24,7 @@ class grammar{
         ////////////////////////////////////////////////////////////////////////
         grammars[3][0] = "<exprline>";
         //::=
-        grammars[3][1] = "<expr>.";
+        grammars[3][1] = "<expr> .";
         ////////////////////////////////////////////////////////////////////////
         grammars[4][0] = "<expr>";
         //::=
@@ -42,31 +44,38 @@ class grammar{
         ////////////////////////////////////////////////////////////////////////
         grammars[8][0] = "<string>";
         //::=
-        grammars[8][1] = "id";
+        grammars[8][1] = "<string> <string>";
         ////////////////////////////////////////////////////////////////////////
-        grammars[9][0] = "<math>";
+        grammars[9][0] = "<string>";
         //::=
-        grammars[9][1] = "integer+integer";
+        grammars[9][1] = "id";
         ////////////////////////////////////////////////////////////////////////
         grammars[10][0] = "<math>";
         //::=
-        grammars[10][1] = "integer-integer";
+        grammars[10][1] = "integer + integer";
         ////////////////////////////////////////////////////////////////////////
         grammars[11][0] = "<math>";
         //::=
-        grammars[11][1] = "integer*integer";
+        grammars[11][1] = "integer - integer";
         ////////////////////////////////////////////////////////////////////////
         grammars[12][0] = "<math>";
         //::=
-        grammars[12][1] = "integer/integer";
+        grammars[12][1] = "integer * integer";
+        ////////////////////////////////////////////////////////////////////////
+        grammars[13][0] = "<math>";
+        //::=
+        grammars[13][1] = "integer / integer";
+        ////////////////////////////////////////////////////////////////////////
+        grammars[14][0] = "<printexpr>";
+        //::=
+        grammars[14][1] = "<printexpr> <string>";
         ////////////////////////////////////////////////////////////////////////
     }
 
-    string check(string expr){
-        for(int i = 0; i < 13; i++){
-            if(grammars[i][1] == expr){
-                return grammars[i][0];
-            }
-        }
+    string getNonTerminal(int id){
+        return grammars[id][0];
     }
-}
+    string getTerminal(int id){
+        return grammars[id][1];
+    }
+};
