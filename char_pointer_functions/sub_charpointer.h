d56058df33c89d58fc75130f8106a1f0d3e16898
emirhanyener <yeneremirhann@gmail.com>
2023-01-15 14:13:32 +0300

    update sub string function

--- char_pointer_functions/sub_charpointer.h
+++ char_pointer_functions/sub_charpointer.h
@@ -12,7 +12,21 @@ char * substr(char * sourcechar, int length){
 	return temp;
 }
 
-char * substrs(char * sourcechar, int length, int start){
+char * substr(const char * sourcechar, int length){
+	int i = 0;
+	if(length >= strlen(sourcechar)){
+		length = strlen(sourcechar);
+	}
+	char * temp = (char *) malloc (sizeof(char) * strlen(sourcechar) - length);
+	for(i = 0; i < length; i++){
+		temp[i] = sourcechar[i];
+	}
+	temp[i] = '\0';
+	
+	return temp;
+}
+
+char * substrs(char * sourcechar, int start, int length){
 	int i = 0;
 	
 	if(start >= strlen(sourcechar)){
@@ -22,7 +36,25 @@ char * substrs(char * sourcechar, int length, int start){
 		length = strlen(sourcechar) - start;
 	}
 	char * temp = (char *) malloc (sizeof(char) * strlen(sourcechar) - length - start);
-	for(i = 0; i < length - start + 1; i++){
+	for(i = 0; i < length; i++){
+		temp[i] = sourcechar[i + start];
+	}
+	temp[i] = '\0';
+	
+	return temp;
+}
+
+char * substrs(const char * sourcechar, int start, int length){
+	int i = 0;
+	
+	if(start >= strlen(sourcechar)){
+		return "";
+	}
+	if(length >= strlen(sourcechar) - start){
+		length = strlen(sourcechar) - start;
+	}
+	char * temp = (char *) malloc (sizeof(char) * strlen(sourcechar) - length - start);
+	for(i = 0; i < length; i++){
 		temp[i] = sourcechar[i + start];
 	}
 	temp[i] = '\0';
